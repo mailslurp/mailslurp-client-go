757f5b5f459fee55d07f7f49bcefff296164b2a6
Jack Mahoney <jackmahoney212@gmail.com>
2019-08-28 15:18:56 +0200

    4.3.3

--- configuration.go
+++ configuration.go
@@ -63,7 +63,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "https://api.mailslurp.com",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/4.3.2/go",
+		UserAgent:     "OpenAPI-Generator/4.3.3/go",
 	}
 	return cfg
 }
